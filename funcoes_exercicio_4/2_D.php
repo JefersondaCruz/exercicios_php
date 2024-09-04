9f4226fda7936aa9ab43f88a6e3ad893c696ba40
Jeferson da Cruz <jefersondacruz40@gmail.com>
2024-09-04 11:13:36 -0300

    NewContents

--- funcoes_exercicio_4/2_D.php
+++ funcoes_exercicio_4/2_D.php
@@ -1,3 +1,25 @@
 <?php       
 
-    
+function SomarNumeros ($a, $b, $c) {
+
+    $menor = min($b, $c);
+    $maior = max($b, $c);
+
+    for ($i = $menor; $i <= $maior; $i++) {
+
+        
+
+        if($a % 2 == 0){
+            
+
+        }
+
+    }
+
+
+    return "A = $a ,B = $menor e C = $maior";
+
+  
+}
+
+echo SomarNumeros (readline("Digite o valor de A: "),readline("Digite o valor de B: "), readline("Digite o valor de c: "));
